5431b620edb6ef429ae5d1e87abfcd62f3597843
Xananax <xananax@yelostudio.com>
2012-08-09 17:49:44 +0300

    added support for exact numbers

--- scripts/labs/stack/test.js
+++ scripts/labs/stack/test.js
@@ -1,14 +1,26 @@
 define(function(){
 
-	function rand(probas){
-		var possibilities = [], n, i;
-  		for(n in probas){
-  			for(i=0;i<probas[n];i++){
-  				possibilities.push(n);
-  			}
-  		}
-  		return possibilities[Math.floor(Math.random() * possibilities.length)];
+	var currN = -1;
+
+	function rand(possibilities,exact){
+		currN++;
+		return exact ? possibilities[currN] : possibilities[Math.floor(Math.random() * possibilities.length)];
 	}
+
+	function createPossibilities(probas){
+		var possibilities = [], n, i;
+		for(n in probas){
+			for(i=0;i<probas[n];i++){
+				possibilities.push(n);
+			}
+		}
+		return shuffle(possibilities);
+	}
+
+	shuffle = function(o){
+		for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+		return o;
+	};
 
 	var ItemsTemplate = Handlebars.compile('{{#each items}}<div class="stackItem {{class}}"></div>{{/each}}');
 
@@ -29,23 +41,27 @@ define(function(){
 		f.onsubmit = function(){
 			var data = [];
 			var total = parseInt(f.lines.value) + parseInt(f.blocks.value) + parseInt(f.squares.value) + parseInt(f.boxes.value);
-			if(total != 100){
+			var exact = f.exact.checked;
+			if(!exact && total != 100){
 				var msg = 'your values do no amount to 100%! they amount to '+total+'.\n';
 				if(total > 100){msg+=' retrieve '+(total-100)+' from somwhere';}
-				else{msg+=' add '+(100-total)+' from somwhere';}
+				else{msg+=' add '+(100-total)+' from somewhere';}
+				msg+=', or check "exact numbers"';
 				alert(msg);
 				return false;
 			}
 			var types = {
-				'landscape2 portrait1 line':parseInt(f.lines.value) || 40
-			,	'landscape1 portrait2 block':parseInt(f.blocks.value) || 40
-			,	'landscape1 portrait1 square':parseInt(f.squares.value) || 10
-			,	'landscape2 portrait2 box':parseInt(f.boxes.value) || 10
+					'landscape1 portrait1 square': 	parseInt(f.squares.value) 	|| 0
+				,	'landscape1 portrait2 block': 	parseInt(f.blocks.value) 	|| 0
+				,	'landscape2 portrait1 line': 	parseInt(f.lines.value) 	|| 0
+				,	'landscape2 portrait2 box': 	parseInt(f.boxes.value) 	|| 0
 			}
-			var n = parseInt(f.number.value) || 50;
+			var n = exact ? total : parseInt(f.number.value) || 50;
+			var possibilities = createPossibilities(types);
 			for(var i=0; i<n;i++){
-				data.push({'class':rand(types)})
+				data.push({'class':rand(possibilities,exact)})
 			}
+			currN=-1;
 			container.innerHTML+=ItemsTemplate({items:data})
 			container.style.height = stack.fit(true).height() + 'px';
 			return false;
